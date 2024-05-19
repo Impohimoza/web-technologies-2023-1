677d5b3333917ef130674cf122d532ecb87a2392
Himozaa <impocanal@gmail.com>
2024-05-19 21:48:43 +0500

    lesson16

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ function getCurrentTime() {
 
     if ($minutes == 1 || $minutes == 21 || $minutes == 31 || $minutes == 41 || $minutes == 51) {
         $minutesText = $minutes . " минута";
-    } elseif (($minutes >= 2 && $minutes <= 4) || ($minutes >= 22 && $minutes <= 24) || ($minutes >= 32 && $minutes <= 34) || ($minutes >= 42 && $minutes <= 44) || ($minutes >= 52 и $minutes <= 54)) {
+    } elseif (($minutes >= 2 && $minutes <= 4) || ($minutes >= 22 && $minutes <= 24) || ($minutes >= 32 && $minutes <= 34) || ($minutes >= 42 && $minutes <= 44) || ($minutes >= 52 && $minutes <= 54)) {
         $minutesText = $minutes . " минуты";
     } else {
         $minutesText = $minutes . " минут";
